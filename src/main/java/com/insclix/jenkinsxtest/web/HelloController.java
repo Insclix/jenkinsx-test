48276e71b77a92e8636d2233bf7516f95c023ff4
randymay <randy.may@blaazinsoftware.com>
2018-05-03 11:06:14 -0400

    Create Rest controller.

---
+++ src/main/java/com/insclix/jenkinsxtest/web/HelloController.java
@@ -0,0 +1,12 @@
+package com.insclix.jenkinsxtest.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String displayMessage() {
+        return "Hello World!";
+    }
+}
